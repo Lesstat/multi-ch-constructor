822adec736fe7dc4ac826aaf30fdcd861e0dbe5f
Florian Barth <barth@fmi.uni-stuttgart.de>
2019-10-15 10:58:04 +0200

    [Graphml] convert boost graph to custom graph

--- src/multi_lib/graphml.hpp
+++ src/multi_lib/graphml.hpp
@@ -14,6 +14,7 @@
 #include <utility>
 
 template <class Graph> class dynamic_property_store {
+  public:
   using vert_id = typename boost::graph_traits<Graph>::vertex_descriptor;
   using edge_id = typename boost::graph_traits<Graph>::edge_descriptor;
 
@@ -33,51 +34,41 @@ template <class Graph> class dynamic_property_store {
   std::map<std::string, edge_bool_map> edge_bool_maps;
   std::map<std::string, edge_info_map> edge_info_maps;
 
-  public:
   boost::shared_ptr<boost::dynamic_property_map> operator()(
-      const std::string& key, const boost::any& any1, const boost::any& any2)
+      const std::string& key, const boost::any& descriptor, const boost::any& value)
   {
+
     boost::shared_ptr<boost::dynamic_property_map> map;
 
-    if (any1.type() == typeid(vert_id)) {
+    if (descriptor.type() == typeid(vert_id)) {
 
-      if (any2.type() == typeid(std::string)) {
-
-        boost::associative_property_map<vert_info_map> ass_map(vert_info_maps[key]);
-        map = boost::shared_ptr<boost::dynamic_property_map>(
-            new boost::detail::dynamic_property_map_adaptor(ass_map));
-      } else if (any2.type() == typeid(double)) {
-
-        boost::associative_property_map<vert_cost_map> ass_map(vert_cost_maps[key]);
-        map = boost::shared_ptr<boost::dynamic_property_map>(
-            new boost::detail::dynamic_property_map_adaptor(ass_map));
-      } else if (any2.type() == typeid(bool)) {
-
-        boost::associative_property_map<vert_bool_map> ass_map(vert_bool_maps[key]);
-        map = boost::shared_ptr<boost::dynamic_property_map>(
-            new boost::detail::dynamic_property_map_adaptor(ass_map));
+      if (value.type() == typeid(std::string)) {
+        map = create_map(key, vert_info_maps);
+      } else if (value.type() == typeid(double)) {
+        map = create_map(key, vert_cost_maps);
+      } else if (value.type() == typeid(bool)) {
+        map = create_map(key, vert_bool_maps);
       }
-    } else if (any1.type() == typeid(edge_id)) {
 
-      if (any2.type() == typeid(std::string)) {
-
-        boost::associative_property_map<edge_info_map> ass_map(edge_info_maps[key]);
-        map = boost::shared_ptr<boost::dynamic_property_map>(
-            new boost::detail::dynamic_property_map_adaptor(ass_map));
-      } else if (any2.type() == typeid(double)) {
-
-        boost::associative_property_map<edge_cost_map> ass_map(edge_cost_maps[key]);
-        map = boost::shared_ptr<boost::dynamic_property_map>(
-            new boost::detail::dynamic_property_map_adaptor(ass_map));
-      } else if (any2.type() == typeid(bool)) {
-
-        boost::associative_property_map<edge_bool_map> ass_map(edge_bool_maps[key]);
-        map = boost::shared_ptr<boost::dynamic_property_map>(
-            new boost::detail::dynamic_property_map_adaptor(ass_map));
+    } else if (descriptor.type() == typeid(edge_id)) {
+      if (value.type() == typeid(std::string)) {
+        map = create_map(key, edge_info_maps);
+      } else if (value.type() == typeid(double)) {
+        map = create_map(key, edge_cost_maps);
+      } else if (value.type() == typeid(bool)) {
+        map = create_map(key, edge_bool_maps);
       }
     }
 
     return map;
+  }
+
+  template <class Map>
+  boost::shared_ptr<boost::dynamic_property_map> create_map(const std::string& key, Map& m)
+  {
+    boost::associative_property_map<typename Map::mapped_type> associative_map(m[key]);
+    return boost::shared_ptr<boost::dynamic_property_map>(
+        new boost::detail::dynamic_property_map_adaptor(associative_map));
   }
 };
 
@@ -89,11 +80,35 @@ Graph read_graphml(const std::string& loadFileName)
   boost::adjacency_list<> boost_graph;
 
   dynamic_property_store<decltype(boost_graph)> store;
-  boost::dynamic_properties graph_properties(store);
+  boost::dynamic_properties graph_properties(
+      [&store](const std::string& key, const boost::any& descriptor, const boost::any& value) {
+        return store(key, descriptor, value);
+      });
 
   boost::read_graphml(graph_stream, boost_graph, graph_properties);
 
-  Graph g { std::vector<Node>(), std::vector<Edge>() };
+  auto vertices = boost::vertices(boost_graph);
+
+  std::vector<Node> nodes;
+  std::vector<Edge> edges;
+  for (auto vert = vertices.first; vert != vertices.second; ++vert) {
+    std::string id = boost::get("id", graph_properties, *vert);
+    nodes.emplace_back(id, NodeId { *vert });
+
+    auto out_edges = boost::out_edges(*vert, boost_graph);
+    for (auto edge = out_edges.first; edge != out_edges.second; ++edge) {
+      auto& my_edge = edges.emplace_back(NodeId { edge->m_source }, NodeId { edge->m_target });
+      Cost c;
+      size_t idx = 0;
+      for (auto it = store.edge_cost_maps.begin(); it != store.edge_cost_maps.end(); ++it) {
+        c.values[idx] = it->second[*edge];
+        ++idx;
+      }
+      my_edge.setCost(c);
+    }
+  }
+
+  Graph g { std::move(nodes), std::move(edges) };
 
   return g;
 };
